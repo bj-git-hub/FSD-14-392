76bda54f9f31457e403de56ba7d3f2c818c8dd4c
bj-git-hub <bj349111@gmail.com>
2026-07-23 15:34:54 +0530

    Addition to Task1

--- Lab1/EventLoop/task1.js
+++ Lab1/EventLoop/task1.js
@@ -1,21 +1,39 @@
+// JS is synchronous and single-threaded.
 const f1 = () => {
-  console.log("f1");
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 ends");
 };
 
 const f2 = () => {
-  console.log("f2");
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
 };
 
 const f3 = () => {
-  console.log("f3");
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 ends");
 };
 
 function main() {
-  console.log("main");
+  console.log("main starts");
   f1();
-  f2();
-  f3();
+  console.log("main running");
   console.log("end main");
+  
 }
 
 main();
+
+// In asynchronous programming, we use event loop to manage the call stack.
+/*
+Asynchronous calls using timers:
+1. setTimeout
+2. setImmediate
+3. process.nextTick
+4. setInterval
+ */
